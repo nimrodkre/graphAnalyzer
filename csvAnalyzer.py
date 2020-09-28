0909b427ea11234c2d02a00d3b2a0104dc9f2216
nimrodkre <nimrod.kremer@mail.huji.ac.il>
2020-09-28 11:10:59 +0300

    jsonAnalyzer now works

--- csvAnalyzer.py
+++ csvAnalyzer.py
@@ -1,7 +1,43 @@
 import pandas as pd
+from graphAnalyzerError import graphAnalyzerError
 
 class csvAnalyzer:
-    def __init__(self, filename, x_column_name, y_column_name):
+    def __init__(self, filename, x_column_name, y_column_name, x_error_name,
+                 y_error_name):
         self.data = pd.read_csv(filename)
         self.x_column_name = x_column_name
         self.y_column_name = y_column_name
+        self.x_error_name = x_error_name
+        self.y_error_name = y_error_name
+
+    def get_x_data(self):
+        try:
+            return self.data[self.x_column_name]
+        except KeyError:
+            raise graphAnalyzerError(
+                "The following column does not exist in given csv: {}".format(
+                    self.x_column_name))
+
+    def get_y_data(self):
+        try:
+            return self.data[self.y_column_name]
+        except KeyError:
+            raise graphAnalyzerError(
+                "The following column does not exist in given csv: {}".format(
+                    self.x_column_name))
+
+    def get_x_error_data(self):
+        try:
+            return self.data[self.x_error_name]
+        except KeyError:
+            raise graphAnalyzerError(
+                "The following column does not exist in given csv: {}".format(
+                    self.x_error_name))
+
+    def get_y_error_data(self):
+        try:
+            return self.data[self.y_error_name]
+        except KeyError:
+            raise graphAnalyzerError(
+                "The following column does not exist in given csv: {}".format(
+                    self.y_error_name))
--- jsonAnalyzer.py
+++ jsonAnalyzer.py
@@ -2,11 +2,11 @@ import json
 from graphAnalyzerError import graphAnalyzerError
 
 
-class jsonAnalyzer:
+class JsonAnalyzer:
     def __init__(self):
         with open("graphDescription.json", "r") as json_data:
             self.graph_data = json.load(json_data, strict=False)
-
+        print(self.graph_data)
         self.excel_file_location = self.graph_data["excel_file_location"]
         self.x_axis_column_name = self.graph_data["x_axis_column_name"]
         self.y_axis_column_name = self.graph_data["y_axis_column_name"]
@@ -25,140 +25,140 @@ class jsonAnalyzer:
 
     @property
     def excel_file_location(self):
-        return self.graph_data["excel_file_location"]
+        return self._excel_file_location
 
     @property
     def x_axis_column_name(self):
-        return self.graph_data["x_axis_column_name"]
+        return self._x_axis_column_name
 
     @property
     def y_axis_column_name(self):
-        return self.graph_data["y_axis_column_name"]
+        return self._y_axis_column_name
 
     @property
     def x_axis_errors_column_name(self):
-        return self.graph_data["x_axis_errors_column_name"]
+        return self._x_axis_errors_column_name
 
     @property
     def y_axis_errors_column_name(self):
-        return self.graph_data["y_axis_errors_column_name"]
+        return self._y_axis_errors_column_name
 
     @property
     def graph_title(self):
-        return self.graph_data["graph_title"]
+        return self._graph_title
 
     @property
     def x_scale_label_title(self):
-        return self.graph_data["x_scale_label_title"]
+        return self._x_scale_label_title
 
     @property
     def x_scale_minimum(self):
-        return self.graph_data["x_scale_minimum"]
+        return self._x_scale_minimum
 
     @property
     def x_scale_maximum(self):
-        return self.graph_data["x_scale_maximum"]
+        return self._x_scale_maximum
 
     @property
     def y_scale_label_title(self):
-        return self.graph_data["x_scale_label_title"]
+        return self._y_scale_label_title
 
     @property
     def y_scale_minimum(self):
-        return self.graph_data["x_scale_minimum"]
+        return self._y_scale_minimum
 
     @property
     def y_scale_maximum(self):
-        return self.graph_data["x_scale_maximum"]
+        return self._y_scale_maximum
 
     @property
     def graph_equation(self):
-        return self.graph_data["graph_equation"]
+        return self._graph_equation
 
     @excel_file_location.setter
     def excel_file_location(self, location):
         if not type(location) == str:
             raise graphAnalyzerError("file location must be a string")
-        self.excel_file_location = location
+        self._excel_file_location = location
 
     @x_axis_column_name.setter
     def x_axis_column_name(self, col_name):
         if not type(col_name) == str:
             raise graphAnalyzerError("x axis column name must be a string")
-        self.x_axis_column_name = col_name
+        self._x_axis_column_name = col_name
 
     @y_axis_column_name.setter
     def y_axis_column_name(self, col_name):
         if not type(col_name) == str:
             raise graphAnalyzerError("y axis column name must be a string")
-        self.y_axis_column_name = col_name
+        self._y_axis_column_name = col_name
 
     @x_axis_errors_column_name.setter
     def x_axis_errors_column_name(self, col_name):
         if not type(col_name) == str:
             raise graphAnalyzerError(
                 "x axis errors column name must be a string")
-        self.x_axis_errors_column_name = col_name
+        self._x_axis_errors_column_name = col_name
 
     @y_axis_errors_column_name.setter
     def y_axis_errors_column_name(self, col_name):
         if not type(col_name) == str:
             raise graphAnalyzerError(
                 "y axis errors column name must be a string")
-        self.y_axis_errors_column_name = col_name
+        self._y_axis_errors_column_name = col_name
 
     @graph_title.setter
     def graph_title(self, title):
         if not type(title) == str:
             raise graphAnalyzerError(
                 "graph title must be a string")
-        self.x_axis_errors_column_name = title
+        self._graph_title = title
 
     @x_scale_label_title.setter
     def x_scale_label_title(self, title):
         if not type(title) == str:
             raise graphAnalyzerError(
                 "x scale label title must be a string")
-        self.x_scale_label_title = title
+        self._x_scale_label_title = title
 
     @x_scale_minimum.setter
     def x_scale_minimum(self, title):
-        if not type(title) == float:
+        if not type(title) in [float, int]:
             raise graphAnalyzerError(
-                "x scale minimum title must be a float")
-        self.x_scale_minimum = title
+                "x scale minimum must be a float")
+        self._x_scale_minimum = title
 
     @x_scale_maximum.setter
     def x_scale_maximum(self, title):
-        if not type(title) == str:
+        if not type(title) in [float, int]:
             raise graphAnalyzerError(
-                "x scale maximum title must be a float")
-        self.x_scale_maximum = title
+                "x scale maximum must be a float")
+        self._x_scale_maximum = title
 
     @y_scale_label_title.setter
     def y_scale_label_title(self, title):
         if not type(title) == str:
             raise graphAnalyzerError(
                 "y scale label title must be a string")
-        self.y_scale_label_title = title
+        self._y_scale_label_title = title
 
     @y_scale_minimum.setter
     def y_scale_minimum(self, title):
-        if not type(title) == float:
+        if not type(title) in [float, int]:
             raise graphAnalyzerError(
-                "y scale minimum title must be a float")
-        self.y_scale_minimum = title
+                "y scale minimum must be a float")
+        self._y_scale_minimum = title
 
     @y_scale_maximum.setter
     def y_scale_maximum(self, title):
-        if not type(title) == str:
+        if not type(title) in [float, int]:
             raise graphAnalyzerError(
-                "y scale maximum title must be a float")
-        self.y_scale_maximum = title
+                "y scale maximum must be a float")
+        self._y_scale_maximum = title
 
     @graph_equation.setter
     def graph_equation(self, equation):
         if not type(equation) == str:
             raise graphAnalyzerError(
                 "equation must be a string")
-        self.graph_equation = equation
+        self._graph_equation = equation
