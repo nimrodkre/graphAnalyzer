0aec7993c8581f7962b8fc4db02b944f64dd417d
nimrodkre <nimrod.kremer@mail.huji.ac.il>
2020-09-29 15:46:25 +0300

    able now to return 0 if no data is given for errors

--- csvAnalyzer.py
+++ csvAnalyzer.py
@@ -29,16 +29,20 @@ class CsvAnalyzer:
                     self.x_column_name))
 
     def get_x_error_data(self):
+        if not self.x_error_name:
+            return [0 for i in range(len(self.get_x_data()))]
         try:
-            return [data for data in self.data[self.x_error_name] if not math.isnan(data)]
+            return [data if not math.isnan(data) else 0 for data in self.data[self.x_error_name]]
         except KeyError:
             raise graphAnalyzerError(
                 "The following column does not exist in given csv: {}".format(
                     self.x_error_name))
 
     def get_y_error_data(self):
+        if not self.y_error_name:
+            return [0 for i in range(len(self.get_x_data()))]
         try:
-            return [data for data in self.data[self.y_error_name] if not math.isnan(data)]
+            return [data if not math.isnan(data) else 0 for data in self.data[self.y_error_name]]
         except KeyError:
             raise graphAnalyzerError(
                 "The following column does not exist in given csv: {}".format(
